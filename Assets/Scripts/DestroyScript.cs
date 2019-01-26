348c2a45b3bfb8050cf25bfdbc1a913b46f870ae
syy1125 <syy1125@hotmail.com>
2019-01-26 13:26:59 -0800

    Disable collider when destroyer makes initial contact

--- Assets/Scripts/DestroyScript.cs
+++ Assets/Scripts/DestroyScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DestroyScript : MonoBehaviour
 {
     private Animator anim;
+    private Collider2D collider;
     
     void Start()
     {
         anim = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,6 +18,7 @@ public class DestroyScript : MonoBehaviour
         if (collision.collider.CompareTag("Destroyer"))
         {
             anim.enabled = true;
+            collider.enabled = false;
         }
     }
 
