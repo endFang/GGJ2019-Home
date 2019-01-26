e81c33518b870a46fe86dadace441d94fbccdf4f
syy1125 <syy1125@hotmail.com>
2019-01-26 12:44:42 -0800

    Implement experimental dialogue system

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public UnityEvent onTriggerEnter;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        onTriggerEnter.Invoke();
+    }
+}
