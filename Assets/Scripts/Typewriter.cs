4e17c581cebcc6f1f9dc0dec7ea610a560a1290e
syy1125 <syy1125@hotmail.com>
2019-01-26 10:45:37 -0800

    Implement typewriter script
    Typewriter script prints out a text character by character.

---
+++ Assets/Scripts/Typewriter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Typewriter : MonoBehaviour
+{
+	private Text text;
+
+	public float startDelay;
+	public float interval;
+	public float endDelay;
+
+	public UnityEvent onEnd;
+
+	private IEnumerator Start()
+	{
+		string originalText = text.text;
+		text.text = "";
+
+		yield return new WaitForSeconds(startDelay);
+
+		foreach (char c in originalText)
+		{
+			text.text += c;
+			yield return new WaitForSeconds(interval);
+		}
+
+		yield return new WaitForSeconds(endDelay);
+
+		onEnd.Invoke();
+	}
+}
