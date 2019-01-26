7ec18469de5d02295087629a89d7ad406b849582
syy1125 <syy1125@hotmail.com>
2019-01-26 10:37:23 -0800

    Implement checkpoint script

---
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		var playerRespawn = other.GetComponent<PlayerRespawn>();
+
+		if (playerRespawn == null) return;
+
+		playerRespawn.checkpointPosition = transform.position;
+	}
+}
