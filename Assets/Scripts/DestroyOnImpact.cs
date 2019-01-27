5cb754e8f9b84a2ea9224244ccc09cbf2c633d04
endFang <dikaif@uci.edu>
2019-01-26 18:14:16 -0800

    - selt-destruction on projectiles

--- Assets/Scripts/DestroyOnImpact.cs
+++ Assets/Scripts/DestroyOnImpact.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class DestroyOnImpact : MonoBehaviour
 {
+
+	private float timer = 0f;
+	private float cooldown = 5f;
+
+	private void Update()
+	{
+		timer += 1f * Time.deltaTime;
+		if (timer > cooldown)
+		{
+			Destroy(gameObject);
+		}
+	}
+
 	private void OnCollisionEnter2D(Collision2D _)
 	{
 		Destroy(gameObject);
