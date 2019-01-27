7982c94ae5d7b5fff265e172e8beeed670cde1e7
syy1125 <syy1125@hotmail.com>
2019-01-26 22:41:43 -0800

    Circle key says pew

--- Assets/Scripts/KeyProviderTrigger.cs
+++ Assets/Scripts/KeyProviderTrigger.cs
@@ -1,14 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyProviderTrigger : MonoBehaviour
 {
 	public PlayerKey key;
+	public GameObject uiPopup;
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (!other.gameObject.CompareTag("Player")) return;
-		
+
 		GameManager.instance.AddKey(key);
-		enabled = false;
+		
+		if (uiPopup != null)
+		{
+			uiPopup.GetComponent<Image>().StartCoroutine(DisplayPopup());
+		}
+
+		Destroy(gameObject);
+	}
+
+	private IEnumerator DisplayPopup()
+	{
+		float startTime = Time.time;
+		RectTransform rt = uiPopup.GetComponent<RectTransform>();
+
+		while (Time.time - startTime < 1)
+		{
+			yield return null;
+			rt.pivot = new Vector2(rt.pivot.x, Mathf.Lerp(0.5f, 0, Time.time - startTime));
+		}
+
+		yield return new WaitForSeconds(1);
+
+		startTime = Time.time;
+
+		while (Time.time - startTime < 1)
+		{
+			yield return null;
+			rt.pivot = new Vector2(rt.pivot.x, Mathf.Lerp(0, 0.5f, Time.time - startTime));
+		}
 	}
 }
