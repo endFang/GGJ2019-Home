2500f180a0d82f52b93adfafc91c785e8a626e86
syy1125 <syy1125@hotmail.com>
2019-01-26 22:57:48 -0800

    Implement player shooting unlock key

---
+++ Assets/Scripts/KeyPopup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class KeyPopup : MonoBehaviour
+{
+	public void DisplayPopup()
+	{
+		StartCoroutine(DisplayPopupCoroutine());
+	}
+
+	private IEnumerator DisplayPopupCoroutine()
+	{
+		float startTime = Time.time;
+		RectTransform rt = GetComponent<RectTransform>();
+
+		while (Time.time - startTime < 1)
+		{
+			yield return null;
+			rt.pivot = new Vector2(rt.pivot.x, Mathf.Lerp(0.5f, 0, Time.time - startTime));
+		}
+
+		yield return new WaitForSeconds(1);
+
+		startTime = Time.time;
+
+		while (Time.time - startTime < 1)
+		{
+			yield return null;
+			rt.pivot = new Vector2(rt.pivot.x, Mathf.Lerp(0, 0.5f, Time.time - startTime));
+		}
+	}
+}
