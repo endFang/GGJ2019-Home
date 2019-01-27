0aeabd79dce745ba98201a08cfbebc74221c33ee
M3RLN <32886755+M3RLN@users.noreply.github.com>
2019-01-26 16:35:29 -0800

    Revert "Merge branch 'master' of https://github.com/syy1125/GGJ2019-Home"
    This reverts commit 467129756aba2dba02636e88707e9e7af6eb5807, reversing
    changes made to 4948a8775a1cee14dff44f7c92ed2c3d62d78d9a.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 	public Transform player;
-	private Vector3 offset;
 
     void Start()
     {
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,44 +5,28 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public Rigidbody2D rgdbdy2;
-    public List<Transform> wayPoints = new List<Transform>();
-    private Transform targetWaypoint;
-    private float minDistance;
+    
+    Transform[] wayPoints;
     public float movespeed;
     public Vector2 maxDistance;
-    private float distance;
-    int wayPointIndex;
+
+    int wayPointIndex = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        wayPointIndex = 0;
-        targetWaypoint = wayPoints[wayPointIndex];
-        minDistance = 1.0f;
+        transform.position = wayPoints[wayPointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         move();
-        distance = Vector2.Distance(transform.position, targetWaypoint.position);
-        Debug.Log("Distance" + distance);
-
-        checkWayPoint(distance);
     }
 
-    void checkWayPoint(float distance)
-    {
-        if(distance <= minDistance)
-        {
-            wayPointIndex = wayPointIndex == 0 ? 1 : 0;  
-            targetWaypoint = wayPoints[wayPointIndex];
-        }
-        
-    }
     void move()
     {
-       transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, movespeed * Time.deltaTime);
+       // transform.position = Vector2.MoveTowards(transform.position, wayPoints[wayPointIndex].transform.position, movespeed * Time.deltaTime);
     }
 }
