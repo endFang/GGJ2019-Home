9afe285114402697347c8ac2d0650b918f2210e5
endFang <dikaif@uci.edu>
2019-01-26 19:06:33 -0800

    - AudioManager is added
    - AudioManager is a singleton obj
    - menu and game scene are assigned BGM

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip menuMusic;
+    public AudioClip gameMusic;
+    public AudioSource musicSource;
+
+    public static AudioManager instance = null;
+    public float lowPitchRange = .3f;
+    public float highPitchRange = 1f;
+
+    private string sceneName;
+    private string prevSceneName;
+    private Scene currentScene;
+    private bool playing = false;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        prevSceneName = sceneName;
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    private void Update()
+    {
+        if (sceneName == "TitleScreen" && !playing)
+        {
+            PlayMusic(menuMusic);
+            playing = true;
+        }
+        else if (sceneName == "Conall Scene" && !playing)
+        {
+            Debug.Log("here");
+            PlayMusic(gameMusic);
+            Debug.Log("playing");
+            playing = true;
+        }
+
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+
+        if (sceneName != prevSceneName)
+        {
+            playing = false;
+            prevSceneName = sceneName;
+        }
+    }
+
+}
