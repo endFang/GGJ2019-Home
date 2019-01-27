458dacae04a397e937545f72a2a2f8db40103338
syy1125 <syy1125@hotmail.com>
2019-01-26 20:10:42 -0800

    Split player controls into individual scripts
    Split `RandomControls`, which controls player movement, jumping, and attack, into three separate scritps `PlayerMove`, `PlayerJump`, and `PlayerAttack`.

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+	public KeyCode keybind = KeyCode.LeftAlt;
+	public GameObject projectilePrefab;
+	public Vector2 spawnOffset;
+	public float projectileSpeed;
+
+	private PlayerMove move;
+
+	private void Start()
+	{
+		move = GetComponent<PlayerMove>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(keybind))
+		{
+			Vector3 spawnPos = transform.position + (Vector3) (spawnOffset * (move.lastMoveDirection + Vector2.up));
+			GameObject projectile = Instantiate(projectilePrefab, spawnPos, Quaternion.identity);
+			projectile.GetComponent<Rigidbody2D>().velocity = move.lastMoveDirection * projectileSpeed;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+	public float jumpStrength = 7f;
+	public Collider2D feetCollider;
+	public LayerMask groundMask;
+	public bool grounded { get; private set; }
+	private Rigidbody2D rb2d;
+
+	private void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space) && grounded)
+		{
+			rb2d.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
+			grounded = false;
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Ground") && !grounded)
+		{
+			Bounds feetBounds = feetCollider.bounds;
+			var feetPosition = new Vector2(transform.position.x, feetBounds.min.y);
+			
+			RaycastHit2D hit = Physics2D.BoxCast(
+				feetPosition,
+				feetBounds.size,
+				0,
+				Vector2.down,
+				0.1f,
+				groundMask
+			);
+
+			if (hit.collider != null && hit.collider.CompareTag("Ground"))
+			{
+				grounded = true;
+			}
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			grounded = false;
+		}
+	}
+}
