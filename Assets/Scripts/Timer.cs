f5c51540844317e688b125396e3911ac65afcb38
syy1125 <syy1125@hotmail.com>
2019-01-27 15:22:27 -0800

    Persist timer in PlayerPrefs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,25 +6,51 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+	public static Timer instance;
+	private bool duplicate;
+
 	private float timeElapsed;
 	private TextMeshProUGUI tmpg;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		timeElapsed = 0.0f;
-        tmpg = this.GetComponent<TextMeshProUGUI>();
-    }
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
+		timeElapsed = PlayerPrefs.HasKey("Timer") ? PlayerPrefs.GetFloat("Timer") : 0.0f;
+		tmpg = GetComponent<TextMeshProUGUI>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 		timeElapsed += Time.deltaTime;
 
 		string minutes = Mathf.Floor(timeElapsed / 60).ToString("00");
 		string seconds = (timeElapsed % 60).ToString("00");
 
-		tmpg.text = string.Format("Timer: {0}:{1}", minutes, seconds);
-        
-    }
+		tmpg.text = $"Timer: {minutes}:{seconds}";
+	}
+
+	public void ClearTimer()
+	{
+		PlayerPrefs.DeleteKey("Timer");
+		timeElapsed = 0;
+	}
+
+	private void OnDestroy()
+	{
+		if (instance != this) return;
+		PlayerPrefs.SetFloat("Timer", timeElapsed);
+	}
 }
