8579f72a2dc5ed4bb8d73b036713c8902f986b2e
Cienev <45864296+Cienev@users.noreply.github.com>
2019-01-26 21:20:30 -0800

    Change PauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private static bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                pauseMenuUI.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                isPaused = true;
+            }
+            else
+            {
+                resume();
+            }
+            
+        } 
+    }
+
+    public void resume()
+    {
+        Debug.Log("resume is pressed");
+        pauseMenuUI.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+
+    }
+    public void quitGame()
+    {
+        Application.Quit();     
+    }
+}
